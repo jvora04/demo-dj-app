24e8ee656e7a94b867970f9db8f836d378a3abbf
jvora04 <96242506+jvora04@users.noreply.github.com>
2022-04-02 19:24:33 +0530

    Update settings.py

--- djrest/djrest/settings.py
+++ djrest/djrest/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+!jftxu^p1oze#doy0ukkuwi$*o2w9)!&%2woy)-+l76nt9=mp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
